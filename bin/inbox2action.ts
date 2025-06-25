233e7f686d609d17dc03ddb5795537058bc8b654
Vinicius Jomori <yuitivinicius@gmail.com>
2025-06-24 23:51:35 -0300

    [feat] dynamic environment configuration via CLI

--- bin/inbox2action.ts
+++ bin/inbox2action.ts
@@ -1,20 +1,24 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { Inbox2ActionStack } from '../lib/inbox2action-stack';
+import * as cdk from 'aws-cdk-lib'
+import { Inbox2ActionStack } from '../lib/inbox2action-stack'
+import * as fs from 'fs';
+import * as path from 'path';
 
 const app = new cdk.App();
-new Inbox2ActionStack(app, 'Inbox2ActionStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+let envName = app.node.tryGetContext('env')
+const config = getConfig(envName)
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new Inbox2ActionStack(app, 'Inbox2Action', {
+  stackName: `inbox2action-stack-${envName}`,
+  envName: envName,
+  ...config
 });
+
+function getConfig(envName: string) {
+  const configPath = path.resolve(__dirname, `../config/${envName}.json`);
+  
+  if (!fs.existsSync(configPath)) throw new Error(`env not found: ${configPath}`);
+
+  const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+  return config
+}
--- lib/inbox2action-stack.ts
+++ lib/inbox2action-stack.ts
@@ -2,16 +2,46 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
+export interface Inbox2ActionProps extends cdk.StackProps {
+  envName: string;
+  username: string;
+  openai: { apiKey: string };
+  aws: {
+    bucketName: string;
+  }
+  clickUp: {
+    apiKey: string,
+    teamId: string,
+    spaceId: string
+  }
+  email: {
+    address: string;
+    name: string;
+    bcc: string
+  }
+}
+
 export class Inbox2ActionStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: Inbox2ActionProps) {
     super(scope, id, props);
 
     const dockerFunc = new lambda.DockerImageFunction(this, "DockerFunc", {
-      functionName: "inbox2action-lambda",
+      functionName: `inbox2action-lambda-${props.envName}`,
       code: lambda.DockerImageCode.fromImageAsset("./image"),
       memorySize: 1024,
       timeout: cdk.Duration.seconds(60),
       architecture: lambda.Architecture.X86_64,
+      environment: {
+        'USERNAME': props.username,
+        'OPENAI_API_KEY': props.openai.apiKey,
+        'AWS_BUCKET_NAME': props.aws.bucketName,
+        'CLICKUP_API_KEY': props.clickUp.apiKey,
+        'CLICKUP_TEAM_ID': props.clickUp.teamId,
+        'CLICKUP_SPACE_ID': props.clickUp.spaceId,
+        'EMAIL_NAME': props.email.name,
+        'EMAIL_ADDRESS': props.email.address,
+        'EMAIL_ADDRESS_BCC': props.email.bcc,
+      }
     });
   }
 }
