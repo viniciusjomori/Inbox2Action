8bd17ae0f321d887f6293ea83faf4e5fcdf7b2b1
Vinicius Jomori <yuitivinicius@gmail.com>
2025-07-12 23:03:06 -0300

    [refactor] remove tmp files for inline images

--- image/src/util/rawemail.py
+++ image/src/util/rawemail.py
@@ -2,12 +2,10 @@ from email import message_from_string
 from email.policy import default
 from email import policy
 from email.parser import BytesParser
-from io import BytesIO
 from email.utils import parseaddr
+from io import BytesIO
+import base64
 from typing import Tuple
-
-from src.util import directory
-from src.util.directory import File
 
 def extract_content(raw_email: str):
     msg = message_from_string(raw_email, policy=default)
@@ -55,7 +53,7 @@ def extract_sender(raw_email: str) -> str:
     name, email = parseaddr(msg.get('From', ''))
     return email
 
-def extract_inline_images(raw_email: str, path="/tmp"):
+def extract_inline_images(raw_email: str):
     raw_bytes = raw_email.encode("utf-8")
     msg = BytesParser(policy=policy.default).parse(BytesIO(raw_bytes))
 
@@ -67,19 +65,12 @@ def extract_inline_images(raw_email: str, path="/tmp"):
         content_id = part.get("Content-ID")
 
         if content_type.startswith("image/") and (content_disposition == "inline" or content_id):
-            ext = content_type.split("/")[-1]
             cid = content_id.strip("<>") if content_id else "inline_image"
-            filename = part.get_filename() or f"{cid}.{ext}"
 
-            image_file = File(
-                name=filename,
-                content=part.get_payload(decode=True),
-                path=path,
-                binary=True
-            )
-            directory.create_file(image_file, path)
+            bytes = base64 \
+                .b64encode(part.get_payload(decode=True)) \
+                .decode('utf-8')
 
-            full_path = f'{path}/{filename}'
-            cids[cid] = directory.to_base64(full_path)
+            cids[cid] = bytes
 
     return cids
