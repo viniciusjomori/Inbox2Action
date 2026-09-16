7b033a0b58976061b893e8f47b6456779f8a8059
viniciusjomori <yuitivinicius@gmail.com>
2026-09-16 11:49:12 -0300

    add: env example

--- bin/inbox2action.ts
+++ bin/inbox2action.ts
@@ -9,7 +9,7 @@ let envName = app.node.tryGetContext('env')
 const config = getConfig(envName)
 
 new Inbox2ActionStack(app, `Inbox2Action[${envName.toUpperCase()}]`, {
-  stackName: `inbox2action-${envName}-stack`,
+  stackName: `Inbox2Action-${envName}-stack`,
   envName: envName,
   ...config
 });
